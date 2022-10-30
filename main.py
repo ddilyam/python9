c3101cf5dacb7dd5ac21489eae0521f7044c0f5b
ddilyam <dilmd@icloud.com>
2022-10-30 17:16:57 +0600

    3

--- main.py
+++ main.py
@@ -18,22 +18,25 @@
 # print(arrayValuesTypes([1, 2, "null", []]))
 ######################################################
 #
-# class Country:
-#     def __init__(self, country, population, area):
-#         self.country = country
-#         self.population = population
-#         self.area = area
-#
-#     def compare_pd(self, country2):
-#         if self.population/self.area > country2.population/country2.area:
-#             print(f'{self.country} has a  larger population density than {country2.country}')
-#         else:
-#             print(f'{self.country} has a  smaller population density than {country2.country}')
-#
-# australia = Country('Australia', 23545500, 7692024)
-# andorra = Country('Andorra', 76098, 468)
-#
-# andorra.compare_pd(australia)
+class Country:
+    def __init__(self, country, population, area):
+        self.country = country
+        self.population = population
+        self.area = area
+
+        while(self.population > 250000000 or self.area > 3000000):
+            self.is_big = True
+
+    def compare_pd(self, country2):
+        if self.population/self.area > country2.population/country2.area:
+            print(f'{self.country} has a  larger population density than {country2.country}')
+        else:
+            print(f'{self.country} has a  smaller population density than {country2.country}')
+
+australia = Country('Australia', 23545500, 7692024)
+andorra = Country('Andorra', 76098, 468)
+
+andorra.compare_pd(australia)
 
 #######################################################
 
