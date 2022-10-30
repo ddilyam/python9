425b442f549ed14e3640b1f47b3cb76edf2aa70e
ddilyam <dilmd@icloud.com>
2022-10-30 19:53:29 +0600

    4

--- main.py
+++ main.py
@@ -18,26 +18,31 @@
 # print(arrayValuesTypes([1, 2, "null", []]))
 ######################################################
 #
-class Country:
-    def __init__(self, country, population, area):
-        self.country = country
-        self.population = population
-        self.area = area
-
-        while(self.population > 250000000 or self.area > 3000000):
-            self.is_big = True
-
-    def compare_pd(self, country2):
-        if self.population/self.area > country2.population/country2.area:
-            print(f'{self.country} has a  larger population density than {country2.country}')
-        else:
-            print(f'{self.country} has a  smaller population density than {country2.country}')
-
-australia = Country('Australia', 23545500, 7692024)
-andorra = Country('Andorra', 76098, 468)
-
-andorra.compare_pd(australia)
+# class Country:
+#     def __init__(self, country, population, area):
+#         self.country = country
+#         self.population = population
+#         self.area = area
+#
+#         while(self.population > 250000000 or self.area > 3000000):
+#             self.is_big = True
+#
+#     def compare_pd(self, country2):
+#         if self.population/self.area > country2.population/country2.area:
+#             print(f'{self.country} has a  larger population density than {country2.country}')
+#         else:
+#             print(f'{self.country} has a  smaller population density than {country2.country}')
+#
+# australia = Country('Australia', 23545500, 7692024)
+# andorra = Country('Andorra', 76098, 468)
+#
+# andorra.compare_pd(australia)
 
 #######################################################
-
+# def sortByLength(arr):
+#     arr.sort(key=len)
+#     return arr
+# print(sortByLength(["Google", "Apple", "Microsoft"]))
+# print(sortByLength(["Leonardo", "Michelangelo", "Raphael", "Donatello"]))
+# print(sortByLength(["Turing", "Einstein", "Jung"]))
 
