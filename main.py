83ba7ca7ff2bf5f85e7f6a8513dd8e12cef44a4e
ddilyam <dilmd@icloud.com>
2022-10-30 17:12:35 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+# def test_jackpot(list):
+#     for i in range(len(list)):
+#         for j in range(i +1, len(list)):
+#             if list[i] in list[j]:
+#                 return True
+#             else:
+#                 return False
+#
+# print(test_jackpot(["@", "@", "@", "@"]))
+# print(test_jackpot(["&&", "&", "&&&", "&&&&"]) )
+#######################################################
+# def arrayValuesTypes(arr):
+#     valuesTypes = []
+#     for i in range(len(arr)):
+#         valuesTypes.append(type(arr[i]))
+#     return valuesTypes
+#
+# print(arrayValuesTypes([1, 2, "null", []]))
+######################################################
+#
+# class Country:
+#     def __init__(self, country, population, area):
+#         self.country = country
+#         self.population = population
+#         self.area = area
+#
+#     def compare_pd(self, country2):
+#         if self.population/self.area > country2.population/country2.area:
+#             print(f'{self.country} has a  larger population density than {country2.country}')
+#         else:
+#             print(f'{self.country} has a  smaller population density than {country2.country}')
+#
+# australia = Country('Australia', 23545500, 7692024)
+# andorra = Country('Andorra', 76098, 468)
+#
+# andorra.compare_pd(australia)
+
+#######################################################
+
+
